aaf96345642301f57e7645165daab8f06dd7672d
UdayAhire21 <udayahire27feb@gmail.com>
2025-10-19 23:30:51 +0530

    initial state

---
+++ src/Components/App.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactCard from './ContactCard';
+import ContactList from './Contactlist';
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+    const addContactHandler = (contact) => {
+        console.log(contact);
+        setContacts((prevContacts)=>[...prevContacts, contact]);
+    };
+
+    useEffect(() => {
+  const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  console.log("Retrieved from storage:", retrieveContacts);
+  if (retrieveContacts) setContacts(retrieveContacts);
+}, []);
+
+useEffect(() => {
+  console.log("Saving to storage:", contacts);
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+}, [contacts]);
+
+
+  return ( 
+     <div className="ui container">
+    <Header />
+    <AddContact addContactHandler={addContactHandler} />
+    <ContactList contact={contacts} />
+    
+  </div>
+  );
+}
+
+export default App;
