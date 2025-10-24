668073ab3149f1273742161aff864133808af931
UdayAhire21 <udayahire27feb@gmail.com>
2025-10-24 22:35:53 +0530

    route import change

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { v4 as uuid } from 'uuid';
 import Header from './Header';
