f0514f776e7256ee430a007fd231b13e9ed56295
UdayAhire21 <udayahire27feb@gmail.com>
2025-10-25 20:57:57 +0530

    chnage in app.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Switch } from 'react-router-dom';
+import {BrowserRouter as Router } from 'react-router-dom'; // Remove Switch from import
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { v4 as uuid } from 'uuid';
@@ -43,18 +43,20 @@ useEffect(() => {
       <Router>
         <Header />
        
-          <Switch>
+          {/* REMOVED: <Switch> tag */}
             
              <Routes>
               
             <Route path="/add" Component={AddContact}/>
-            <Route path="/" exact Component={ContactList}/>
+            {/* Note: The 'exact' prop is no longer needed in v6 */}
+            <Route path="/" Component={ContactList}/>
       
             </Routes>
-          </Switch>
+          {/* REMOVED: </Switch> tag */}
        
        
-    {/* <AddContact addContactHandler={addContactHandler} />
+    {/* The old way of using components is commented out below:
+    <AddContact addContactHandler={addContactHandler} />
     <ContactList contact={contacts} getContactId={removeContactHandler}/> */}
     
       </Router>
